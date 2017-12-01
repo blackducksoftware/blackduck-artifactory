4e2043ea46958afa5de5d3ede7d7abbc3cc935f9
Richard Otte <rotte@blackducksoftware.com>
2017-12-01 14:52:14 -0500

    Cache inspector now puts properties on the artifacts it inspects

--- src/main/groovy/com/blackducksoftware/integration/hub/artifactory/inspect/blackDuckCacheInspector.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/artifactory/inspect/blackDuckCacheInspector.groovy
@@ -1,11 +1,12 @@
 package com.blackducksoftware.integration.hub.artifactory.inspect
 
-import org.artifactory.fs.FileInfo
 import org.artifactory.fs.FileLayoutInfo
-import org.artifactory.fs.ItemInfo
-import org.artifactory.fs.StatsInfo
 import org.artifactory.md.Properties
 import org.artifactory.repo.RepoPath
+
+import com.blackducksoftware.integration.hub.bdio.model.Forge
+import com.blackducksoftware.integration.hub.bdio.model.externalid.ExternalId
+import com.blackducksoftware.integration.hub.bdio.model.externalid.ExternalIdFactory
 
 
 executions {
@@ -16,7 +17,7 @@ executions {
             Set<RepoPath> repoPaths = searchForRepoPaths(params['repos'][0], params['patterns'][0])
             message = ''
             for(RepoPath repoPath: repoPaths) {
-                message += getForgeInformation(repoPath) + '\n'
+                populateHubOriginId(repoPath)
             }
         }
     }
@@ -34,31 +35,45 @@ private Set<RepoPath> searchForRepoPaths(String repos, String patterns) {
     repoPaths.toSet()
 }
 
-private String getForgeInformation(RepoPath repoPath) {
+private void populateHubOriginId(RepoPath repoPath) {
+    ExternalId externalId = constructExternalIdFromRepoPath(repoPath)
+    if (externalId) {
+        String hubOriginId = externalId.createHubOriginId()
+        repositories.setProperty(repoPath, 'blackduck.hubOriginId', externalId.createHubOriginId())
+    }
+}
+
+private ExternalId constructExternalIdFromRepoPath(RepoPath repoPath) {
+    ExternalIdFactory externalIdFactory = new ExternalIdFactory()
+    String packageType = repositories.getRepositoryConfiguration(repoPath.getRepoKey()).getPackageType();
     FileLayoutInfo fileLayoutInfo = repositories.getLayoutInfo(repoPath)
     Properties properties = repositories.getProperties(repoPath)
-    StatsInfo stats = repositories.getStats(repoPath)
-    FileInfo fileInfo = repositories.getFileInfo(repoPath)
-    ItemInfo itemInfo = repositories.getItemInfo(repoPath)
-    String name;
-    String version;
-    String forge = repositories.getRepositoryConfiguration(repoPath.getRepoKey()).getPackageType();
-    switch (forge) {
-        case 'maven':
-            name = fileLayoutInfo.module
-            version = fileLayoutInfo.baseRevision
-            break;
-        case 'npm':
-            name = properties['npm.name'][0]
-            version = properties['npm.version'][0]
-            break;
-        case 'nuget':
-            name = properties['nuget.id'][0]
-            version = properties['nuget.version'][0]
-            break;
-        default:
-            return "${forge}:\n${properties.dump()}\n${repoPath.dump()}\n${fileLayoutInfo.dump()}\n${stats.dump()}\n${fileInfo.dump()}\n${itemInfo.dump()}\n"
-            break;
+
+    if ('nuget'.equals(packageType)) {
+        String name = properties['nuget.id'][0]
+        String version = properties['nuget.version'][0]
+        return externalIdFactory.createNameVersionExternalId(Forge.NUGET, name, version)
     }
-    return "${forge}:${name}:${version}"
+
+    if ('npm'.equals(packageType)) {
+        String name = properties['npm.name'][0]
+        String version = properties['npm.version'][0]
+        return externalIdFactory.createNameVersionExternalId(Forge.NPM, name, version)
+    }
+
+    if ('maven'.equals(packageType) || 'gradle'.equals(packageType)) {
+        String name = fileLayoutInfo.module
+        String version = fileLayoutInfo.baseRevision
+        String group = fileLayoutInfo.organization
+        return externalIdFactory.createMavenExternalId(group, name, version)
+    }
+
+
+    if ('gems'.equals(packageType)) {
+        String name = fileLayoutInfo.module
+        String version = fileLayoutInfo.baseRevision
+        return externalIdFactory.createNameVersionExternalId(Forge.RUBYGEMS, name, version)
+    }
+
+    return null
 }
