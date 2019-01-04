da6f066667c6056c5e7c85a2be78dc55f6373490
Jake Mathews <jake.mathews.email@gmail.com>
2019-01-04 10:21:53 -0500

    Added AnalyticsModuleConfigTest

---
+++ blackduck-artifactory-common/src/test/groovy/com/synopsys/integration/blackduck/artifactory/modules/analytics/AnalyticsModuleConfigTest.java
@@ -0,0 +1,41 @@
+package com.synopsys.integration.blackduck.artifactory.modules.analytics;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.synopsys.integration.util.BuilderStatus;
+
+class AnalyticsModuleConfigTest {
+    private AnalyticsModuleConfig validAnalyticsModuleConfig;
+    private AnalyticsModuleConfig invalidAnalyticsModuleConfig;
+
+    @BeforeEach
+    void init() {
+        validAnalyticsModuleConfig = new AnalyticsModuleConfig(true);
+        invalidAnalyticsModuleConfig = new AnalyticsModuleConfig(null);
+    }
+
+    @Test
+    void validate() {
+        final BuilderStatus validBuilderStatus = new BuilderStatus();
+        validAnalyticsModuleConfig.validate(validBuilderStatus);
+        Assert.assertEquals(0, validBuilderStatus.getErrorMessages().size());
+
+        final BuilderStatus invalidBuilderStatus = new BuilderStatus();
+        invalidAnalyticsModuleConfig.validate(invalidBuilderStatus);
+        Assert.assertEquals(1, invalidBuilderStatus.getErrorMessages().size());
+    }
+
+    @Test
+    void isEnabled() {
+        Assert.assertTrue(validAnalyticsModuleConfig.isEnabled());
+        Assert.assertFalse(invalidAnalyticsModuleConfig.isEnabled());
+    }
+
+    @Test
+    void isEnabledUnverified() {
+        Assert.assertTrue(validAnalyticsModuleConfig.isEnabledUnverified());
+        Assert.assertNull(invalidAnalyticsModuleConfig.isEnabledUnverified());
+    }
+}
