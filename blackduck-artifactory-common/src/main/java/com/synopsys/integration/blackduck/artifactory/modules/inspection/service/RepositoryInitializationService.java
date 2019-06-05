56d9403bc9249793b5c582e566791ab4c19570e1
Jake Mathews <jake.mathews.email@gmail.com>
2019-06-05 14:23:27 -0400

    fix(inspection) Added the ability for repo-initialization to retry.

--- blackduck-artifactory-common/src/main/java/com/synopsys/integration/blackduck/artifactory/modules/inspection/service/RepositoryInitializationService.java
+++ blackduck-artifactory-common/src/main/java/com/synopsys/integration/blackduck/artifactory/modules/inspection/service/RepositoryInitializationService.java
@@ -69,8 +69,8 @@ public class RepositoryInitializationService {
 
     private void initializeRepository(final RepoPath repoKeyPath) throws FailedInspectionException {
         final String repoKey = repoKeyPath.getRepoKey();
-        final Optional<InspectionStatus> repoInspectionStatus = inspectionPropertyService.getInspectionStatus(repoKeyPath);
-        if (repoInspectionStatus.isPresent()) {
+
+        if (inspectionPropertyService.shouldRetryInspection(repoKeyPath)) {
             // If an inspection status is present, we don't need to do a BOM upload. A failure will be cleared automatically or by a user.
             logger.debug(String.format("Not performing repo initialization on '%s' because it has already been initialized.", repoKey));
             return;
