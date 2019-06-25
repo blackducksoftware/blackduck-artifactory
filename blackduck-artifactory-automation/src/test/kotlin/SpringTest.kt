d31bb835d97fcea0f2c9c3987e40cbb283d29147
Jake Mathews <jake.mathews.email@gmail.com>
2019-06-25 12:05:09 -0400

    fix(build) Added exclusion of automation tests by default.

--- blackduck-artifactory-automation/src/test/kotlin/SpringTest.kt
+++ blackduck-artifactory-automation/src/test/kotlin/SpringTest.kt
@@ -1,9 +1,14 @@
+import com.synopsys.integration.blackduck.artifactory.automation.Application
 import com.synopsys.integration.blackduck.artifactory.automation.ApplicationConfiguration
+import org.junit.jupiter.api.Tag
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
 @ContextConfiguration(classes = [ApplicationConfiguration::class])
+@Tag("automation")
 abstract class SpringTest {
-
+    @Autowired
+    lateinit var application: Application
 }
