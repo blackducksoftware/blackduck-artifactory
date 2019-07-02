db483c3a4f4ed46999e0ba4e9c1213b0aaa430f1
Jake Mathews <jake.mathews.email@gmail.com>
2019-07-02 11:20:22 -0400

    feat(package-type): Package types now report whether they are automated or not.

--- blackduck-artifactory-automation/src/main/kotlin/com/synopsys/integration/blackduck/artifactory/automation/artifactory/api/PackageType.kt
+++ blackduck-artifactory-automation/src/main/kotlin/com/synopsys/integration/blackduck/artifactory/automation/artifactory/api/PackageType.kt
@@ -3,31 +3,32 @@ package com.synopsys.integration.blackduck.artifactory.automation.artifactory.ap
 interface PackageType {
     val packageType: String
     val remoteUrl: String
+    val automated: Boolean
 
-    enum class Defaults(override val packageType: String, override val remoteUrl: String) : PackageType {
-        BOWER("bower", "https://github.com/"),
+    enum class Defaults(override val packageType: String, override val remoteUrl: String, override val automated: Boolean = false) : PackageType {
+        BOWER("bower", "https://github.com/", true),
         CHEF("chef", "https://supermarket.chef.io"),
         COCOAPODS("cocoapods", "https://github.com/"),
-        COMPOSER("composer", "https://github.com/"),
+        COMPOSER("composer", "https://github.com/", true),
         CONAN("conan", "https://conan.bintray.com"),
         CONDA("conda", "https://repo.anaconda.com/pkgs/free"),
-        CRAN("cran", "https://cran.r-project.org/"),
+        CRAN("cran", "https://cran.r-project.org/", true),
         DEBIAN("debian", "http://archive.ubuntu.com/ubuntu/"),
-        // DOCKER("com/synopsys/integration/blackduck/artifactory/automation/docker", "https://registry-1.docker.io/"),
-        GEMS("gems", "https://rubygems.org/"),
+        // DOCKER("docker", "https://registry-1.docker.io/", false), Unsupported.
+        GEMS("gems", "https://rubygems.org/", true),
         // GENERIC("generic", "" ), Needs a remote URL. This PackageType should be created manually.
         // GITLFS("gitlfs", ""), Needs a remote URL. This PackageType should be created manually.
         GO("go", "https://gocenter.io/"),
-        GRADLE("gradle", "https://jcenter.bintray.com"),
+        GRADLE("gradle", "https://jcenter.bintray.com", true),
         HELM("helm", "https://storage.googleapis.com/kubernetes-charts"),
         IVY("ivy", "https://jcenter.bintray.com"),
-        MAVEN("maven", "https://jcenter.bintray.com"),
-        NPM("npm", "https://registry.npmjs.org"),
-        NUGET("nuget", "https://www.nuget.org/"),
+        MAVEN("maven", "https://jcenter.bintray.com", true),
+        NPM("npm", "https://registry.npmjs.org", true),
+        NUGET("nuget", "https://www.nuget.org/", true),
         // OPKG("opkg", ""), Needs a remote URL. This PackageType should be created manually.
         // P2("p2", ""), Needs a remote URL. This PackageType should be created manually.
         PUPPET("puppet", "https://forgeapi.puppetlabs.com/"),
-        PYPI("pypi", "https://files.pythonhosted.org"),
+        PYPI("pypi", "https://files.pythonhosted.org", true),
         RPM("rpm", "http://mirror.centos.org/centos/"),
         SBT("sbt", "https://jcenter.bintray.com"),
         // VAGRANT("vagrant", ""), In doc, but not creatable via UI as of Artifactory-6.10.3
