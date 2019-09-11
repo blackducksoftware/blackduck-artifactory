4ace28f759f2d825da228efa601cdda4b9be62b0
Jake Mathews <jake.mathews.email@gmail.com>
2019-09-11 15:02:37 -0400

    test(conda) Added more unit tests for CondaExternalIdExtractor.

--- blackduck-artifactory-common/src/test/kotlin/unit/modues/inspection/externalid/conda/CondaExternalIdExtractorTest.kt
+++ blackduck-artifactory-common/src/test/kotlin/unit/modues/inspection/externalid/conda/CondaExternalIdExtractorTest.kt
@@ -1,6 +1,7 @@
-package com.synopsys.integration.blackduck.artifactory.modules.inspection.externalid.conda
+package unit.modues.inspection.externalid.conda
 
 import com.synopsys.integration.bdio.model.externalid.ExternalIdFactory
+import com.synopsys.integration.blackduck.artifactory.modules.inspection.externalid.conda.CondaExternalIdExtractor
 import com.synopsys.integration.blackduck.artifactory.modules.inspection.model.SupportedPackageType
 import org.artifactory.repo.RepoPath
 import org.junit.jupiter.api.Assertions
@@ -28,11 +29,23 @@ internal class CondaExternalIdExtractorTest {
 
     @Test
     fun extractInvalidFormatExternalId() {
+        val parentRepoPath = mock(RepoPath::class.java)
+        val repoPath: RepoPath = mock(RepoPath::class.java)
+        mockWhen(repoPath.name).thenReturn("numpy-1.13.1-py27-_0.tar.bz2")
+        mockWhen(repoPath.parent).thenReturn(parentRepoPath)
+        mockWhen(parentRepoPath.name).thenReturn("linux-64")
 
+        val externalId = condaExternalIdExtractor.extractExternalId(supportedPackageType, repoPath)
+        Assertions.assertFalse(externalId.isPresent)
     }
 
     @Test
     fun extractNoParentRepoPath() {
+        val repoPath: RepoPath = mock(RepoPath::class.java)
+        mockWhen(repoPath.name).thenReturn("numpy-1.13.1-py27_0.tar.bz2")
+        mockWhen(repoPath.parent).thenReturn(null)
 
+        val externalId = condaExternalIdExtractor.extractExternalId(supportedPackageType, repoPath)
+        Assertions.assertFalse(externalId.isPresent)
     }
 }
