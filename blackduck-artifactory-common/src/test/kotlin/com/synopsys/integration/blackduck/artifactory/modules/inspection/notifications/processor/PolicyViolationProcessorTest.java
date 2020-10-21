42be1084e863adc509544f66d0be1b18aa75baea
Jake Mathews <jake.mathews.email@gmail.com>
2020-10-21 15:41:07 -0400

    test(notifications): Added test for notification processors.

---
+++ blackduck-artifactory-common/src/test/kotlin/com/synopsys/integration/blackduck/artifactory/modules/inspection/notifications/processor/PolicyViolationProcessorTest.java
@@ -0,0 +1,58 @@
+package com.synopsys.integration.blackduck.artifactory.modules.inspection.notifications.processor;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.artifactory.repo.RepoPath;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.synopsys.integration.blackduck.api.enumeration.PolicySeverityType;
+import com.synopsys.integration.blackduck.api.generated.enumeration.PolicySummaryStatusType;
+import com.synopsys.integration.blackduck.api.manual.component.ComponentVersionStatus;
+import com.synopsys.integration.blackduck.api.manual.component.PolicyInfo;
+import com.synopsys.integration.blackduck.api.manual.component.RuleViolationNotificationContent;
+import com.synopsys.integration.blackduck.api.manual.view.RuleViolationNotificationUserView;
+import com.synopsys.integration.blackduck.artifactory.PluginRepoPathFactory;
+import com.synopsys.integration.blackduck.artifactory.modules.inspection.notifications.RepositoryProjectNameLookup;
+import com.synopsys.integration.exception.IntegrationException;
+
+class PolicyViolationProcessorTest {
+    @Test
+    void processPolicyViolationNotifications() throws IntegrationException {
+        ProcessorUtil processorUtil = Mockito.mock(ProcessorUtil.class);
+        Mockito.when(processorUtil.fetchApprovalStatus(Mockito.any())).thenReturn(PolicySummaryStatusType.IN_VIOLATION);
+
+        RepositoryProjectNameLookup repositoryFilter = Mockito.mock(RepositoryProjectNameLookup.class);
+        RepoPath repoPath = new PluginRepoPathFactory(false).create("repo-1");
+        Mockito.when(repositoryFilter.getRepoKeyPath(Mockito.any(), Mockito.any())).thenReturn(Optional.of(repoPath));
+
+        PolicyViolationProcessor policyViolationProcessor = new PolicyViolationProcessor(processorUtil);
+
+        RuleViolationNotificationUserView notificationUserView = new RuleViolationNotificationUserView();
+        RuleViolationNotificationContent content = new RuleViolationNotificationContent();
+        content.setProjectName("project-name");
+        content.setProjectVersionName("project-version-name");
+        content.setPolicyInfos(Collections.singletonList(new PolicyInfo()));
+        ComponentVersionStatus componentVersionStatus = new ComponentVersionStatus();
+        componentVersionStatus.setComponentName("component-name");
+        componentVersionStatus.setComponentVersionName("component-version-name-1.0");
+        componentVersionStatus.setBomComponentVersionPolicyStatus("https://synopsys.com/bomComponentVersionPolicyStatus");
+        content.setComponentVersionStatuses(Collections.singletonList(componentVersionStatus));
+        notificationUserView.setContent(content);
+
+        List<ProcessedPolicyNotification> processedPolicyNotifications = policyViolationProcessor.processPolicyViolationNotifications(Collections.singletonList(notificationUserView), repositoryFilter);
+
+        Assertions.assertEquals(1, processedPolicyNotifications.size());
+
+        ProcessedPolicyNotification processedPolicyNotification = processedPolicyNotifications.get(0);
+
+        Assertions.assertEquals("component-name", processedPolicyNotification.getComponentName());
+        Assertions.assertEquals("component-version-name-1.0", processedPolicyNotification.getComponentVersionName());
+        Assertions.assertEquals(Collections.singletonList(repoPath), processedPolicyNotification.getAffectedRepoKeyPaths());
+        Assertions.assertEquals(Collections.singletonList(PolicySeverityType.UNSPECIFIED), processedPolicyNotification.getPolicyStatusReport().getPolicySeverityTypes());
+        Assertions.assertEquals(PolicySummaryStatusType.IN_VIOLATION, processedPolicyNotification.getPolicyStatusReport().getPolicySummaryStatusType());
+    }
+}
---
+++ blackduck-artifactory-common/src/test/kotlin/com/synopsys/integration/blackduck/artifactory/modules/inspection/notifications/processor/VulnerabiityProcessorTest.java
@@ -0,0 +1,81 @@
+package com.synopsys.integration.blackduck.artifactory.modules.inspection.notifications.processor;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.artifactory.repo.RepoPath;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.synopsys.integration.blackduck.api.generated.component.ResourceLink;
+import com.synopsys.integration.blackduck.api.generated.component.ResourceMetadata;
+import com.synopsys.integration.blackduck.api.generated.view.ComponentVersionView;
+import com.synopsys.integration.blackduck.api.generated.view.VulnerabilityView;
+import com.synopsys.integration.blackduck.api.manual.component.AffectedProjectVersion;
+import com.synopsys.integration.blackduck.api.manual.component.VulnerabilityNotificationContent;
+import com.synopsys.integration.blackduck.api.manual.view.VulnerabilityNotificationUserView;
+import com.synopsys.integration.blackduck.artifactory.PluginRepoPathFactory;
+import com.synopsys.integration.blackduck.artifactory.modules.inspection.notifications.RepositoryProjectNameLookup;
+import com.synopsys.integration.blackduck.artifactory.modules.inspection.notifications.model.VulnerabilityAggregate;
+import com.synopsys.integration.blackduck.service.BlackDuckService;
+import com.synopsys.integration.exception.IntegrationException;
+
+class VulnerabiityProcessorTest {
+    @Test
+    void processVulnerabilityNotifications() throws IntegrationException {
+        BlackDuckService blackDuckService = Mockito.mock(BlackDuckService.class);
+        ComponentVersionView componentVersionView = new ComponentVersionView();
+        componentVersionView.setVersionName("component-version-name-1.0");
+        Mockito.when(blackDuckService.getResponse("component/version/url", ComponentVersionView.class)).thenReturn(componentVersionView);
+
+        ResourceLink vulnerabilityResourceLink = new ResourceLink();
+        vulnerabilityResourceLink.setHref("vulnerability/link/url");
+        vulnerabilityResourceLink.setRel(ComponentVersionView.VULNERABILITIES_LINK);
+        ResourceMetadata resourceMetadata = new ResourceMetadata();
+        resourceMetadata.setLinks(Collections.singletonList(vulnerabilityResourceLink));
+        componentVersionView.setMeta(resourceMetadata);
+
+        List<VulnerabilityView> vulnerabilityViews = Arrays.asList(createView("HIGH"), createView("MEDIUM"), createView("LOW"), createView("HIGH"));
+        Mockito.when(blackDuckService.getAllResponses("vulnerability/link/url", VulnerabilityView.class)).thenReturn(vulnerabilityViews);
+
+        RepositoryProjectNameLookup repositoryFilter = Mockito.mock(RepositoryProjectNameLookup.class);
+        RepoPath repoPath = new PluginRepoPathFactory(false).create("repo-1");
+
+        Mockito.when(repositoryFilter.getRepoKeyPath(Mockito.any())).thenReturn(Optional.of(repoPath));
+
+        VulnerabilityProcessor vulnerabilityProcessor = new VulnerabilityProcessor(blackDuckService);
+        VulnerabilityNotificationUserView notificationUserView = new VulnerabilityNotificationUserView();
+        VulnerabilityNotificationContent content = new VulnerabilityNotificationContent();
+        AffectedProjectVersion affectedProjectVersion = new AffectedProjectVersion();
+        affectedProjectVersion.setProjectName("project-name");
+        affectedProjectVersion.setProjectVersionName("project-version-name");
+        content.setAffectedProjectVersions(Collections.singletonList(affectedProjectVersion));
+        content.setComponentName("component-name");
+        content.setComponentVersion("component/version/url");
+
+        notificationUserView.setContent(content);
+
+        List<ProcessedVulnerabilityNotification> processedVulnerabilityNotifications = vulnerabilityProcessor.processVulnerabilityNotifications(Collections.singletonList(notificationUserView), repositoryFilter);
+
+        Assertions.assertEquals(1, processedVulnerabilityNotifications.size());
+
+        ProcessedVulnerabilityNotification processedVulnerabilityNotification = processedVulnerabilityNotifications.get(0);
+
+        Assertions.assertEquals("component-name", processedVulnerabilityNotification.getComponentName());
+        Assertions.assertEquals("component-version-name-1.0", processedVulnerabilityNotification.getComponentVersionName());
+        Assertions.assertEquals(Collections.singletonList(repoPath), processedVulnerabilityNotification.getAffectedRepoKeyPaths());
+        VulnerabilityAggregate aggregate = processedVulnerabilityNotification.getVulnerabilityAggregate();
+        Assertions.assertEquals(2, aggregate.getHighSeverityCount());
+        Assertions.assertEquals(1, aggregate.getMediumSeverityCount());
+        Assertions.assertEquals(1, aggregate.getLowSeverityCount());
+    }
+
+    private VulnerabilityView createView(String severity) {
+        VulnerabilityView vulnerabilityView = new VulnerabilityView();
+        vulnerabilityView.setSeverity(severity);
+        return vulnerabilityView;
+    }
+}
