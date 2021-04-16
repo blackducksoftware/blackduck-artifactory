ecb81264d2b49c3a0b311d91ed907034bba45cf1
Jake Mathews <jake.mathews.email@gmail.com>
2021-04-16 15:53:44 -0400

    style(license): Added missing license headers.

--- blackduck-artifactory-common/src/test/kotlin/com/synopsys/integration/blackduck/artifactory/modules/inspection/notifications/processor/ProcessorUtilTest.java
+++ blackduck-artifactory-common/src/test/kotlin/com/synopsys/integration/blackduck/artifactory/modules/inspection/notifications/processor/ProcessorUtilTest.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-artifactory-common
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.blackduck.artifactory.modules.inspection.notifications.processor;
 
 import java.util.Arrays;
