84dc58094e9659a61a08d23e3be23b3eac203dcc
Jake Mathews <jake.mathews.email@gmail.com>
2021-04-16 15:49:43 -0400

    style(license): Added missing license headers.

--- blackduck-artifactory-common/src/main/java/com/synopsys/integration/blackduck/artifactory/modules/inspection/externalid/composer/model/FileNamePieces.java
+++ blackduck-artifactory-common/src/main/java/com/synopsys/integration/blackduck/artifactory/modules/inspection/externalid/composer/model/FileNamePieces.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-artifactory-common
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.blackduck.artifactory.modules.inspection.externalid.composer.model;
 
 public class FileNamePieces {
