a14e3b41135cc3cf9ed94377a2e9a4fc2c9fda0e
Builder Service <serv-builder@int-ubuntu-jenkins02.dc1.lan>
2021-05-17 15:01:49 -0400

    Using the next snapshot post release 8.0.0-SIGQA4-SNAPSHOT

--- blackduck-artifactory-common/src/main/java/com/synopsys/integration/blackduck/artifactory/modules/inspection/notifications/deleteme/TempBlackDuckUrlSearchTerm.java
+++ blackduck-artifactory-common/src/main/java/com/synopsys/integration/blackduck/artifactory/modules/inspection/notifications/deleteme/TempBlackDuckUrlSearchTerm.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-artifactory-common
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.blackduck.artifactory.modules.inspection.notifications.deleteme;
 
 // TODO: This is the copied from the blackduck-common:55.0.0 implementation prior to its release and should be removed when possible. - JM 05/2021
